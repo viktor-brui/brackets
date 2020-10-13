f450d3a622fb5ab64ec3b88f1e97830e5bfdd7fa
viktor <viktor-brui@mail.ru>
2020-10-13 21:57:55 +0300

    init: create brackets

---
+++ src/index.js
@@ -0,0 +1,26 @@
+module.exports = function check(str, bracketsConfig) {
+    const findBrackets = (newStr) => {
+        for (i = 0; i < newStr.length; i++) {
+            for (j = 0; j < bracketsConfig.length; j++)
+                if (newStr[i] == bracketsConfig[j][0]) {
+                    if (newStr[i + 1] == bracketsConfig[j][1]) {
+                        return i;
+                    }
+                }
+        }
+        return false;
+    };
+    str = str.split("");
+    if (str.length % 2 == 1) {
+        return false;
+    } else {
+        let find = findBrackets(str);
+        while (find === 0 || find > 0) {
+            find = findBrackets(str);
+            if (find > 0 || find === 0) {
+                str.splice(find, 2);
+            }
+        }
+        return str.length == 0;
+    }
+};
